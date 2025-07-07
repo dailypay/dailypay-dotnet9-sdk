e7ffc6675967ae528da775f1a8a328011bb9f7d9
santiagoveloDP <santiagoveloDP@users.noreply.github.com>
2025-07-07 14:23:17 +0000

    chore: regenerate .NET 9 SDK

--- src/DailyPay/People.cs
+++ src/DailyPay/People.cs
@@ -70,8 +70,8 @@ namespace DailyPay
     {
         public SDKConfig SDKConfiguration { get; private set; }
         private const string _language = "csharp";
-        private const string _sdkVersion = "0.0.7";
-        private const string _sdkGenVersion = "2.648.7";
+        private const string _sdkVersion = "0.0.8";
+        private const string _sdkGenVersion = "2.651.2";
         private const string _openapiDocVersion = "3.0.0-beta01";
 
         public People(SDKConfig config)
