ea9297cdc2f3e1d9be023b5b61589308999ea46a
ChristianSavinoDP <ChristianSavinoDP@users.noreply.github.com>
2025-07-21 21:21:24 +0000

    chore: regenerate .NET 9 SDK

--- src/DailyPay/SDK/DotNet9/Jobs.cs
+++ src/DailyPay/SDK/DotNet9/Jobs.cs
@@ -83,8 +83,8 @@ namespace DailyPay.SDK.DotNet9
     {
         public SDKConfig SDKConfiguration { get; private set; }
         private const string _language = "csharp";
-        private const string _sdkVersion = "0.0.8";
-        private const string _sdkGenVersion = "2.657.1";
+        private const string _sdkVersion = "0.0.9";
+        private const string _sdkGenVersion = "2.660.0";
         private const string _openapiDocVersion = "3.0.0-beta01";
 
         public Jobs(SDKConfig config)
