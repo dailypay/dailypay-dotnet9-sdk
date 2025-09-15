531d47eff027c6e412b429a9298c10cb467e6814
ChristianSavinoDP <ChristianSavinoDP@users.noreply.github.com>
2025-09-15 14:18:20 +0000

    chore: regenerate .NET 9 SDK

--- src/DailyPay/SDK/DotNet9/Health.cs
+++ src/DailyPay/SDK/DotNet9/Health.cs
@@ -59,8 +59,8 @@ namespace DailyPay.SDK.DotNet9
     {
         public SDKConfig SDKConfiguration { get; private set; }
         private const string _language = "csharp";
-        private const string _sdkVersion = "0.2.0";
-        private const string _sdkGenVersion = "2.674.3";
+        private const string _sdkVersion = "0.3.1";
+        private const string _sdkGenVersion = "2.698.4";
         private const string _openapiDocVersion = "3.0.0-beta01";
 
         public Health(SDKConfig config)
--- src/DailyPay/SDK/DotNet9/SDKConfig.cs
+++ src/DailyPay/SDK/DotNet9/SDKConfig.cs
@@ -52,7 +52,7 @@ namespace DailyPay.SDK.DotNet9
                 },
             };
             Version = null;
-            UserAgent = "speakeasy-sdk/csharp 0.2.0 2.674.3 3.0.0-beta01 DailyPay.SDK.DotNet9";
+            UserAgent = "speakeasy-sdk/csharp 0.3.1 2.698.4 3.0.0-beta01 DailyPay.SDK.DotNet9";
             SecuritySource = null;
             Hooks = new SDKHooks();
             RetryConfig = null;
